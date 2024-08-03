fdf8c4064ffa44bf875477954166a440baabf8e9
Frankie <45204032+frankie6589@users.noreply.github.com>
2024-08-03 10:46:14 +0800

    update

---
+++ week2.py
@@ -0,0 +1,57 @@
+import pandas as pd
+
+d1 = {'A':[1,0,0,1], 'B':[0,1,1,0], 'C':[0,1,1,0], 'D':[1,0,0,1]}
+
+# the prints are for testing output
+
+# Problem 1
+def makeDataFrame(data):
+	# YOUR SOLUTION STARTS HERE
+	
+	return
+    # YOUR SOLUTION ENDS HERE
+
+print(makeDataFrame(d1))
+
+# Problem 2a
+def load():
+	#SOLUTION START( ~ 1 line of code)
+	return
+	#SOLUTION END
+
+
+df = load() # use df for the remaining problems
+
+# Problem 2b
+def makeCategory():
+	#SOLUTION START( ~ 1-2 lines of code)
+	return
+	#SOLUTION END
+
+print(makeCategory())
+
+# Problem 2c
+def totalDislike():
+	#SOLUTION START( ~ 1-2 line of code)
+	return
+	#SOLUTION END
+
+print(totalDislike())
+
+
+# Problem 2d
+def totalBookmarks():
+	#SOLUTION START( ~ 1-2 line of code)
+	return
+	#SOLUTION END
+
+print(totalBookmarks())
+
+
+# Problem 2e
+def extractReview():
+	#SOLUTION START( ~ 1-2 line of code)
+	return
+	#SOLUTION END
+
+print(extractReview())
